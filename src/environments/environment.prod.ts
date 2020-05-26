6584acf7d61270c06caadd25ca3e57016cd9e914
Jorge <jorgeelcampos59@gmail.com>
2020-05-26 14:34:49 -0500

    h

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API: {
+    EndPoint: {
+      Northwind: 'http://localhost:81/Proyecto.Peimbert/v1/Analysis/Northwind/'
+    }
+  }
 };
